72b511400879814bf7fc8150d4b794687aa2e3e7
Jasonsze <jasonsze@tamu.edu>
2023-11-05 06:17:01 -0600

    added auth config file for auth0

--- 01-login/server.py
+++ 01-login/server.py
@@ -1,26 +1,10 @@
-"""Python Flask WebApp Auth0 integration example
-"""
-
 import json
 from os import environ as env
 from urllib.parse import quote_plus, urlencode
 
-import openai
 from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
-from flask import Flask, redirect, render_template, session, url_for
-
-openai.api_key = "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"
-
-
-
-response = openai.Completion.create(
-  model="gpt-3.5-turbo-instruct",
-  prompt="Write a tagline for an ice cream shop."
-)
-
-generated_text = response.choices[0].text
-print(generated_text)
+from flask import Flask, jsonify, request
 
 ENV_FILE = find_dotenv()
 if ENV_FILE:
@@ -28,10 +12,6 @@ if ENV_FILE:
 
 app = Flask(__name__)
 app.secret_key = env.get("APP_SECRET_KEY")
-
-def createQuestion(type):
-    prompt
-    
 
 oauth = OAuth(app)
 
@@ -45,51 +25,13 @@ oauth.register(
     server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration',
 )
 
+# Define an API endpoint to retrieve user information
+@app.route("/api/user")
+def get_user():
+    user_info = session.get("user")
+    return jsonify(user_info)
 
-# Controllers API
-@app.route("/")
-def home():
-    return render_template(
-        "App.js",
-        session=session.get("user"),
-        pretty=json.dumps(session.get("user"), indent=4),
-    )
-
-
-@app.route("/callback", methods=["GET", "POST"])
-def callback():
-    
-    token = oauth.auth0.authorize_access_token()
-    parsed_token = token['userinfo']['sub']
-    print(parsed_token[parsed_token.index('|')+1:])
-    
-    session["user"] = token
-    return redirect("/")
-
-
-@app.route("/login")
-def login():
-    return oauth.auth0.authorize_redirect(
-        redirect_uri=url_for("callback", _external=True)
-    )
-
-
-@app.route("/logout")
-def logout():
-    session.clear()
-    return redirect(
-        "https://"
-        + env.get("AUTH0_DOMAIN")
-        + "/v2/logout?"
-        + urlencode(
-            {
-                "returnTo": url_for("home", _external=True),
-                "client_id": env.get("AUTH0_CLIENT_ID"),
-            },
-            quote_via=quote_plus,
-        )
-    )
-
+# Define other API endpoints as needed for your application
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=env.get("PORT", 3000))
